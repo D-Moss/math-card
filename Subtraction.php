e10d3af60b3493231e697a6ded0333f60f5ec2df
Diona Moss <dionamoss@gmail.com>
2023-03-31 00:55:23 -0400

    fixed subtraction

--- Subtraction.php
+++ Subtraction.php
@@ -11,8 +11,8 @@
 
 	<h1>
 		<?php
-			$num1 = rand(1,5);
-			$num2 = rand(5,10);
+			$num1 = rand(1,10);
+			$num2 = rand(1,10);
 
 			echo $num1 . " - " . $num2;
 
@@ -21,7 +21,7 @@
 	<br/><br/>
 	<?php
 	if (!$_POST['answer']) {} else {
-		$correct_answer = $_POST['num1'] + $_POST['num2'];
+		$correct_answer = $_POST['num1'] - $_POST['num2'];
 
 			if ($correct_answer == $_POST['answer']) {
 				echo "Correct! " . $_POST['num1'] . " - " . $_POST['num2'] . " = " . $_POST['answer'];} else {
@@ -36,7 +36,7 @@
 
 	<br/><br/><br/><br/>
 
-	<form method="post" action="/Subtraction.php">
+	<form method="post" action="Subtraction.php">
 				<input name="num1" type="hidden" value="<?php echo $num1 ?>">
 				<input name="num2" type="hidden" value="<?php echo $num2 ?>">
 		Answer: <input name="answer"> <button>Submit</button> <button>New Card</button>
