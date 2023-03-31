dfa4a040d78c737ec48c523f89e43c42a027f3da
Diona Moss <dionamoss@gmail.com>
2023-03-30 23:21:26 -0400

    flashcard

---
+++ index2.php
@@ -0,0 +1,20 @@
+
+<html>
+	<head>
+		<title>Math Flashcards</title>
+	</head>
+	<body>
+
+	<h1>Session Variables</h1>
+
+
+	<?php
+	
+		echo $_SESSION['fav_pizza'];
+		
+	?>		
+
+
+
+	</body>
+</html>
