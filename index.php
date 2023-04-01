e1275b20fdfb7570f77c6703b8254cb9bcc35abd
Diona Moss <dionamoss@gmail.com>
2023-03-31 20:41:48 -0400

    added multiplication

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<center>
-		<a href="index.php">Addition</a> | <a href="Subtraction.php">Subtraction</a>
+		<a href="index.php">Addition</a> | <a href="Subtraction.php">Subtraction</a> | <a href="Multiplication.php">Multiplication</a>
 		<br/><br/>
 	<h1>Addition</h1>
 
